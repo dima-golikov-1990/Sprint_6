1660aef4f5eb863cce81f6f0020a76f294ede058
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2023-11-11 23:09:24 +0300

    Убрал еще одну лишнюю пустую строку.

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
-
     def __init__(self, driver):
         self.driver = driver
         self.base_url = 'https://qa-scooter.praktikum-services.ru/'
